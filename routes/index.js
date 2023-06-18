e32a2cfb5148a7b8245a5eeb41c4d2d7136cc258
LeeBoonKong <lee.boon.kong@hotmail.com>
2023-06-18 17:13:16 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/generateHash', function(req, res, next) {
+});
+
+router.get('/checkHash', function (req, res, next) {
+});
+
+
+
+module.exports = router;
